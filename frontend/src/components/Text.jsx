d2be31bead81a78c4a3de26e3a4c46fae07e3b0a
Ylva Landoff Lindberg <ylva.lindberg@oletergroup.se>
2024-12-12 14:40:58 +0100

    animation added

--- frontend/src/components/Text.jsx
+++ frontend/src/components/Text.jsx
@@ -1,18 +1,57 @@
+
 import React from "react";
-import { StyledP } from "../style/GlobalStylComponents";
+import styled, { keyframes } from "styled-components";
 
 
-export const Text = ({ text, index, randomNumber }) => {
-  const fontSize = 40 - (index * 2);
+const floatAnimation = keyframes`
+  0% {
+    transform: translate(0, 0);
+  }
+  25% {
+    transform: translate(20px, -30px);
+  }
+  50% {
+    transform: translate(-30px, 20px);
+  }
+  75% {
+    transform: translate(30px, 30px);
+  }
+  100% {
+    transform: translate(0, 0);
+  }
+`;
 
-  let fontColors = ["red", "blue", "green", "yellow", "orange", "purple", "pink"];
-  const randomColor = fontColors[Math.floor(Math.random() * fontColors.length)];
 
+const FloatingText = styled.div`
+  position: absolute;
+  font-size: ${(props) => props.fontSize || "16px"};
+  color: ${(props) => props.color || "white"};
+  font-weight: bold;
+  font-family: "Courier New", Courier, monospace;
+  animation: ${floatAnimation} ${(props) => props.duration}s infinite ease-in-out;
+  top: ${(props) => props.startY}%;
+  left: ${(props) => props.startX}%;
+`;
 
+export const Text = ({ text, index }) => {
+
+  const fontSize = 60 - (index * 2);
+// let fontColors = ["red", "blue", "green", "yellow", "orange", "purple", "pink"];
+// const randomColor = fontColors[Math.floor(Math.random() * fontColors.length)];
+
+  const randomStartX = Math.random() * 80; // Start position (left) between 0-80% of screen
+  const randomStartY = Math.random() * 80; // Start position (top) between 0-80% of screen
+  const randomDuration = 5 + Math.random() * 5; // Animation duration between 5-10 seconds
 
   return (
-    <StyledP fontSize={`${fontSize}px`} fontColor={randomColor} >
-      {text} {index}
-    </StyledP>
+    <FloatingText
+      // color={randomColor}
+      fontSize={`${fontSize}px`}
+      startX={randomStartX}
+      startY={randomStartY}
+      duration={randomDuration}
+    >
+      {text}
+    </FloatingText>
   );
 };
--- frontend/src/style/GlobalStylComponents.jsx
+++ frontend/src/style/GlobalStylComponents.jsx
@@ -2,52 +2,29 @@ import styled from 'styled-components';
 
 
 export const MainContainer = styled.div`
+    position: relative;
     display: flex;
     flex-direction: column;
     height: 100vh;
     background: linear-gradient(to top left, #0099ff 0%, #003366 100%);
+    overflow: hidden; 
 `;
 
 export const InnerContainer = styled.div`
-    display: flex;
-    flex-direction: column;
+    position: relative;
+    flex-grow: 1;
     overflow: hidden;
 `;
 
-export const StyledP = styled.p`
-    position: absolute;
-    animation: moveAround 10s infinite alternate;
-
-    @keyframes moveAround {
-        0% {
-            top: 0;
-            left:${(props) => props.randomLeft1};
-        }
-      
-        50% {
-            top: 50%;
-            left: ${(props) => props.randomLeft2};
-        }
-
-        100% {
-            top: 0;
-            left: ${(props) => props.randomLeft3};
-        }
-    }
-    font-size: ${(props) => props.fontSize || '12px'};
-    color: ${(props) => props.fontColor || 'black'};
-    padding: 0;
-    margin: 0;
-    font-weight: bold;
-    font-family: 'Courier New', Courier, monospace;
-`;
 
 export const StyledInput = styled.input`
     height: 40px;
     width: 200px;
-    border: 2px solid black;
+    border: 2px solid white;
     border-radius: 8px;    
     @media (min-width: 768px) {
+    padding-left: 5px;
+    width: 300px;
             height: 50px;
             font-size: 20px;
         }
@@ -76,8 +53,10 @@ export const StyledForm = styled.form`
 export const StyledBtn = styled.button`
     height: 40px;
     padding: 0 20px;
-    border: 2px solid black;
+    border: 2px solid white;
     border-radius: 8px;
+    color: white;
+    background: transparent;
     font-size: 16px;
         @media (min-width: 768px) {
             height: 50px;
