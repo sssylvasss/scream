b8f946354ab9245b84457ef6a2c6729df1d0b571
Ylva Landoff Lindberg <ylva.lindberg@oletergroup.se>
2024-12-17 14:57:43 +0100

    imports

--- frontend/src/components/Input.jsx
+++ frontend/src/components/Input.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { StyledBtn, StyledForm, StyledInput } from "../style/GlobalStylComponents";
-import { bannedWords } from "../config/bannedWords";
+import { bannedWords } from "../utils/bannedWords";
 const API_URL = import.meta.env.VITE_API_URL;
 
 export const Input = ({ onScreamPosted }) => {
