47c000a965a697cc64e3cfb1a14c96f16aa9ef05
Ylva Landoff Lindberg <ylva.lindberg@oletergroup.se>
2024-12-17 14:49:51 +0100

    in frontend

--- frontend/src/components/Input.jsx
+++ frontend/src/components/Input.jsx
@@ -1,13 +1,23 @@
 import React, { useState } from "react";
 import { StyledBtn, StyledForm, StyledInput } from "../style/GlobalStylComponents";
-
+import { bannedWords } from "../config/bannedWords";
 const API_URL = import.meta.env.VITE_API_URL;
 
 export const Input = ({ onScreamPosted }) => {
   const [scream, setScream] = useState("");
 
+  const containsBannedWords = (text) => {
+    const lowerText = text.toLowerCase();
+    return bannedWords.some((word) => lowerText.includes(word));
+  };
+  
+
   const handleSubmit = (e) => {
     e.preventDefault();
+    if (containsBannedWords(userInput)) {
+      alert("Your message contains inappropriate language.");
+      return;
+    }
     fetch(`${API_URL}/screams`, {
       method: 'POST',
       headers: {
